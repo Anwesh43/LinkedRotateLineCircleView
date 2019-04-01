020170df8a49aa697f3cb428e955dd5ac9daa7d7
Anwesh43 <anweshthecool0@gmail.com>
2019-04-01 10:33:00 +0530

    adding RotateLineCircleView class

--- rotatelinecircleview/src/main/java/com/anwesh/uiprojects/rotatelinecircleview/RotateLineCircleView.kt
+++ rotatelinecircleview/src/main/java/com/anwesh/uiprojects/rotatelinecircleview/RotateLineCircleView.kt
@@ -72,3 +72,19 @@ fun Canvas.drawRLCNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class RotateLineCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
