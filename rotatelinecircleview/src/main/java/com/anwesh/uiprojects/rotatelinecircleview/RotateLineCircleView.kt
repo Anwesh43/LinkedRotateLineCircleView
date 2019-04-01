f85c953f7d67faee91afc8b76b6fec690ffc1d53
Anwesh43 <anweshthecool0@gmail.com>
2019-04-01 11:22:07 +0530

    adding drawing fixes

--- rotatelinecircleview/src/main/java/com/anwesh/uiprojects/rotatelinecircleview/RotateLineCircleView.kt
+++ rotatelinecircleview/src/main/java/com/anwesh/uiprojects/rotatelinecircleview/RotateLineCircleView.kt
@@ -22,6 +22,7 @@ val foreColor : Int = Color.parseColor("#43A047")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val deg : Float = 45f
 val rFactor : Float = 3.5f
+val parts : Int = 2
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -29,36 +30,41 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int): Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
-fun Int.sjf() : Float = 1f - (this % 2)
+fun Int.sjf() : Float = 1f - 2 * (this % 2)
 
 fun Canvas.drawExpandingCircle(x : Float, r : Float, paint : Paint) {
     save()
     translate(x, 0f)
+    paint.style = Paint.Style.FILL
+    paint.color = backColor
+    drawCircle(0f, 0f, r, paint)
+    paint.style = Paint.Style.STROKE
+    paint.color = foreColor
     drawCircle(0f, 0f, r, paint)
     restore()
 }
 
 fun Canvas.drawLine(size : Float, paint : Paint) {
-    drawLine(0f, 0f, Math.max(0f, size), 0f, paint)
+    drawLine(0f, 0f, size, 0f, paint)
 }
 
 fun Canvas.drawRotateLineCircle(i : Int, sc1 : Float, sc2 : Float, size : Float, paint : Paint) {
     val sci1 : Float = sc2.divideScale(i, lines)
-    val sci11 : Float = sci1.divideScale(0, 2)
-    val sci12 : Float = sci1.divideScale(1, 2)
+    val sci11 : Float = sci1.divideScale(0, parts)
+    val sci12 : Float = sci1.divideScale(1, parts)
     val x : Float = size * sci11
     val r : Float = (size / rFactor) * sc1
     save()
     rotate(deg * sci12 * i.sjf())
-    drawLine((x - r), paint)
-    drawExpandingCircle(x, r, paint)
+    drawExpandingCircle(x * i.sjf(), r, paint)
+    drawLine(Math.max((x - r), 0f) * i.sjf(), paint)
     restore()
 }
 
 fun Canvas.drawRLCNode(i : Int, scale : Float, paint : Paint) {
     val w : Float = width.toFloat()
     val h : Float = height.toFloat()
-    val gap : Float = w / (nodes + 1)
+    val gap : Float = h / (nodes + 1)
     val size : Float = gap / sizeFactor
     val sc1 : Float = scale.divideScale(0, 2)
     val sc2 : Float = scale.divideScale(1, 2)
@@ -66,9 +72,9 @@ fun Canvas.drawRLCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     paint.strokeCap = Paint.Cap.ROUND
     save()
-    translate(gap * (i + 1), h / 2)
+    translate(w / 2, gap * (i + 1))
     for (j in 0..(lines - 1)) {
-        drawRotateLineCircle(0, sc1, sc2, size, paint)
+        drawRotateLineCircle(j, sc1, sc2, size, paint)
     }
     restore()
 }
@@ -93,7 +99,7 @@ class RotateLineCircleView(ctx : Context) : View(ctx) {
     data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
 
         fun update(cb : (Float) -> Unit) {
-            scale += scale.updateValue(dir, lines, 1)
+            scale += scale.updateValue(dir, 1, lines * parts)
             if (Math.abs(scale - prevScale) > 1) {
                 scale = prevScale + dir
                 dir = 0f
@@ -144,7 +150,7 @@ class RotateLineCircleView(ctx : Context) : View(ctx) {
         private var prev : RLCNode? = null
 
         init {
-
+            addNeighbor()
         }
 
         fun addNeighbor() {
@@ -232,7 +238,7 @@ class RotateLineCircleView(ctx : Context) : View(ctx) {
         fun create(activity : Activity) : RotateLineCircleView {
             val view : RotateLineCircleView = RotateLineCircleView(activity)
             activity.setContentView(view)
-            return view 
+            return view
         }
     }
 }
