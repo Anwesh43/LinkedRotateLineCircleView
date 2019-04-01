cbcb9529a56c7942d587da4fb82b9b946f4eeb49
Anwesh43 <anweshthecool0@gmail.com>
2019-04-01 10:47:41 +0530

    adding RLCNode class

--- rotatelinecircleview/src/main/java/com/anwesh/uiprojects/rotatelinecircleview/RotateLineCircleView.kt
+++ rotatelinecircleview/src/main/java/com/anwesh/uiprojects/rotatelinecircleview/RotateLineCircleView.kt
@@ -135,4 +135,48 @@ class RotateLineCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RLCNode(var i : Int, val state : State = State()) {
+
+        private var next : RLCNode? = null
+        private var prev : RLCNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = RLCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRLCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RLCNode {
+            var curr : RLCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
