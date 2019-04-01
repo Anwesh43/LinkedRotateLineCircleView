3f3d29a9d9091ed91abb0f5ff8ce26c2dd378f30
Anwesh43 <anweshthecool0@gmail.com>
2019-04-01 09:52:11 +0530

    adding luibraries and constants

--- rotatelinecircleview/src/main/java/com/anwesh/uiprojects/rotatelinecircleview/RotateLineCircleView.kt
+++ rotatelinecircleview/src/main/java/com/anwesh/uiprojects/rotatelinecircleview/RotateLineCircleView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.rotatelinecircleview
 /**
  * Created by anweshmishra on 01/04/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 5
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#43A047")
+val backColor : Int = Color.parseColor("#BDBDBD")
