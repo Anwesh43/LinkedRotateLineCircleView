4aaf6910f36f308381d20cb1708bffa494fdcf56
Anwesh43 <anweshthecool0@gmail.com>
2019-04-01 11:22:58 +0530

    adding deelay as constant

--- rotatelinecircleview/src/main/java/com/anwesh/uiprojects/rotatelinecircleview/RotateLineCircleView.kt
+++ rotatelinecircleview/src/main/java/com/anwesh/uiprojects/rotatelinecircleview/RotateLineCircleView.kt
@@ -23,6 +23,7 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 val deg : Float = 45f
 val rFactor : Float = 3.5f
 val parts : Int = 2
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -122,7 +123,7 @@ class RotateLineCircleView(ctx : Context) : View(ctx) {
             if (animated) {
                 cb()
                 try {
-                    Thread.sleep(50)
+                    Thread.sleep(delay)
                     view.invalidate()
                 } catch(ex : Exception) {
 
